38b123c708bf05aa8fdcaddeb901b9ccb2cf72cd
mddeloarhosen68 <62032396+mddeloarhosen68@users.noreply.github.com>
2020-05-08 02:59:34 +0600

    Add files via upload

---
+++ Comment.py
@@ -0,0 +1,15 @@
+#Comments
+#This is a comment
+#written in
+#more than just one line
+
+
+print("Hello, World!")
+
+"""
+This is a comment
+written in
+more than just one line
+"""
+
+print("Hello, World!")
